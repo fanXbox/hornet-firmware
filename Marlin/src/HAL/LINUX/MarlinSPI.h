75fd988af921eb5dfeadf02cf0c7275316836a28
fanXbox <107966612+fanXbox@users.noreply.github.com>
2023-09-18 04:26:04 +0200

    Add files via upload

--- Marlin/src/HAL/LINUX/MarlinSPI.h
+++ Marlin/src/HAL/LINUX/MarlinSPI.h
@@ -1,6 +1,6 @@
 /**
  * Marlin 3D Printer Firmware
- * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2021 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
@@ -19,14 +19,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
-#ifdef __PLAT_LINUX__
+#pragma once
 
-#include <pinmapping.h>
+#include <SPI.h>
 
-#include "../../../gcode/parser.h"
-
-int16_t PARSED_PIN_INDEX(const char code, const int16_t dval) {
-  return parser.intval(code, dval);
-}
-
-#endif // __PLAT_LINUX__
+using MarlinSPI = SPIClass;
