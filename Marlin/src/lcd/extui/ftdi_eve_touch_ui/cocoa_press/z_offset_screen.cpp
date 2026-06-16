5c7c0032f2100aa442822d22ca35a51252e13e6f
fanXbox <107966612+fanXbox@users.noreply.github.com>
2026-06-16 22:11:51 +0200

    Add files via upload

--- Marlin/src/lcd/extui/ftdi_eve_touch_ui/cocoa_press/z_offset_screen.cpp
+++ Marlin/src/lcd/extui/ftdi_eve_touch_ui/cocoa_press/z_offset_screen.cpp
@@ -1,6 +1,6 @@
-/**********************
- * media_filereader.h *
- **********************/
+/***********************
+ * z_offset_screen.cpp *
+ ***********************/
 
 /****************************************************************************
  *   Written By Mark Pelletier  2017 - Aleph Objects, Inc.                  *
@@ -20,27 +20,40 @@
  *   location: <https://www.gnu.org/licenses/>.                             *
  ****************************************************************************/
 
-#pragma once
+#include "../config.h"
+#include "../screens.h"
+#include "../screen_data.h"
 
-#include "../../../../inc/MarlinConfigPre.h"
+#ifdef COCOA_Z_OFFSET_SCREEN
 
-#if HAS_MEDIA
-  #include "../../../../sd/SdFile.h"
-  #include "../../../../sd/cardreader.h"
-#endif
+#include "z_offset_wizard.h"
 
-class MediaFileReader {
-  private:
-    #if HAS_MEDIA
-      MediaFile root, file;
-    #endif
+using namespace FTDI;
+using namespace ExtUI;
+using namespace Theme;
 
-  public:
-    bool open(const char *filename);
-    int16_t read(void *buff, size_t bytes);
-    uint32_t size();
-    void rewind();
-    void close();
+void ZOffsetScreen::onRedraw(draw_mode_t what) {
+  widgets_t w(what);
+  w.precision(2, BaseNumericAdjustmentScreen::DEFAULT_MIDRANGE).units(GET_TEXT_F(MSG_UNITS_MM));
 
-    static int16_t read(void *obj, void *buff, size_t bytes);
-};
+  w.heading(                  GET_TEXT_F(MSG_BABYSTEP_PROBE_Z));
+  w.color(z_axis).adjuster(4, GET_TEXT_F(MSG_ZPROBE_ZOFFSET), getZOffset_mm());
+  w.increments();
+  w.button(2, GET_TEXT_F(MSG_PROBE_WIZARD), !isPrinting());
+}
+
+bool ZOffsetScreen::onTouchHeld(uint8_t tag) {
+  const int16_t steps =   TERN0(BABYSTEPPING, mmToWholeSteps(getIncrement(), Z));
+  const float increment = TERN(BABYSTEPPING, mmFromWholeSteps(steps, Z), getIncrement());
+  switch (tag) {
+    case 2: ZOffsetWizard::runWizard(); break;
+    case 4: UI_DECREMENT(ZOffset_mm); TERN(BABYSTEPPING, babystepAxis_steps(-steps, Z), UNUSED(steps)); break;
+    case 5: UI_INCREMENT(ZOffset_mm); TERN(BABYSTEPPING, babystepAxis_steps( steps, Z), UNUSED(steps)); break;
+    default:
+      return false;
+  }
+  SaveSettingsDialogBox::settingsChanged();
+  return true;
+}
+
+#endif // COCOA_Z_OFFSET_SCREEN
