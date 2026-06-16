6e5076f67a292284401e4dd6df9820a5de671e65
fanXbox <107966612+fanXbox@users.noreply.github.com>
2026-06-16 21:23:35 +0200

    Add files via upload

--- buildroot/share/PlatformIO/scripts/STM32F1_create_variant.py
+++ buildroot/share/PlatformIO/scripts/STM32F1_create_variant.py
@@ -1,34 +1,43 @@
-import os,shutil
-from SCons.Script import DefaultEnvironment
-from platformio import util
+#
+# STM32F1_create_variant.py
+#
+import pioutil
+if pioutil.is_pio_build():
+    from pathlib import Path
 
-def copytree(src, dst, symlinks=False, ignore=None):
-    for item in os.listdir(src):
-        s = os.path.join(src, item)
-        d = os.path.join(dst, item)
-        if os.path.isdir(s):
-            shutil.copytree(s, d, symlinks, ignore)
-        else:
-            shutil.copy2(s, d)
+    source_root_str = "buildroot/share/PlatformIO/variants"
+    source_root = Path(source_root_str)
+    assert source_root.is_dir()
 
-env = DefaultEnvironment()
-platform = env.PioPlatform()
-board = env.BoardConfig()
+    env = pioutil.env
+    board = env.BoardConfig()
+    variant = board.get("build.variant")
 
-FRAMEWORK_DIR = platform.get_package_dir("framework-arduinoststm32-maple")
-assert os.path.isdir(FRAMEWORK_DIR)
-assert os.path.isdir("buildroot/share/PlatformIO/variants")
+    source_dir = source_root / variant
+    assert source_dir.is_dir()
 
-variant = board.get("build.variant")
-variant_dir = os.path.join(FRAMEWORK_DIR, "STM32F1", "variants", variant)
+    if True:
+        # Copying to the platform folder is still needed by STM32F1 (Maple).
+        # The alternative code below comes close. See if you can solve it!
+        platform = env.PioPlatform()
+        FRAMEWORK_DIR = Path(platform.get_package_dir("framework-arduinoststm32-maple"))
+        assert FRAMEWORK_DIR.is_dir()
 
-source_dir = os.path.join("buildroot/share/PlatformIO/variants", variant)
-assert os.path.isdir(source_dir)
+        variant_dir = FRAMEWORK_DIR / "STM32F1/variants" / variant
 
-if os.path.isdir(variant_dir):
-    shutil.rmtree(variant_dir)
+        if variant_dir.is_dir():
+            import shutil
+            shutil.rmtree(variant_dir)
 
-if not os.path.isdir(variant_dir):
-    os.mkdir(variant_dir)
+        if not variant_dir.is_dir():
+            variant_dir.mkdir()
 
-copytree(source_dir, variant_dir)
+        import marlin
+        marlin.copytree(source_dir, variant_dir)
+
+    else:
+
+        # The following almost works, but __start__ (from wirish/start.S) is not seen by common.inc
+        board.update("build.variants_dir", source_root_str);
+        src = str(source_dir)
+        env.Append(BUILD_FLAGS=[f"-I{src}", f"-L{src}/ld"])  # Add include path for variant
